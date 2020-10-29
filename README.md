7c7c5cc0ed8ea95f0aca979aac5c9c260ca310ca
Juan Jose Mendoza Santana <me@jnjo.io>
2020-10-29 19:13:05 +0000

    initial commit
    Initial version of the script doing the minimum required from it

---
+++ README.md
@@ -0,0 +1,45 @@
+# Rights Manager Scaled Ingestion
+
+This scripts allows partners to upload semi-automatically a big number of videos.
+
+## Prerequisites
+
+- Python 3: [https://www.python.org/downloads/](https://www.python.org/downloads/)
+
+- Pip: [https://pip.pypa.io/en/stable/installing/](https://pip.pypa.io/en/stable/installing/)
+
+- Make sure you can install packages using pip:  [https://packaging.python.org/tutorials/installing-packages/]()
+
+## Instructions
+
+1. Make sure you have all the prerequisites installed and configured
+
+2. Download and unzip the zip file containing the script and the configuration file
+
+3. Open a terminal/console and use the command `cd path/to/files` to navigate to the directory where you have placed the script and the other files.
+
+4. Run the following command to install the requirements for the script: `pip install -r requirements.txt`
+
+5. Verify your default configuration in the [defaults.yaml](defaults.yaml) file.
+
+6. Run the script from the terminal using the command `python3 batcher.py`.
+
+7. When propmpt in the terminal, introduce the full path of the directory containing your videos.
+
+
+## Default configurations
+
+You can review and update your default configuration for the videos in the [defaults.yaml](defaults.yaml) file. This file must contain the following fields with the corresponding values
+
+```YAML
+territories: [CountryCode1, CountryCode2]
+description: Place your generic description for the videos here
+category: WEB |
+monitoring_type: VIDEO_AND_AUDIO | VIDEO | AUDIO
+match_rule_id: []
+reference_disabled: TRUE | FALSE
+whitelisted_fb_ids: []
+whitelisted_ig_ids: []
+tags: []
+action: CREATE | UPDATE | DELETE
+```
---
+++ batcher.py
@@ -0,0 +1,109 @@
+import os
+import csv
+import yaml
+from pathlib import Path
+
+
+def is_video_file(file_name):
+    f = os.path.splitext(file_name)
+    valid_video_formats = [
+        ".3g2", ".3gp", ".3gpp",
+        ".asf", ".avi",
+        ".dat", ".divx", ".dv",
+        ".f4v", ".flv",
+        ".gif",
+        ".m2ts", ".m4v", ".mkv", ".mod",
+        ".mov", ".mp4", ".mpe", ".mpeg",
+        ".mpeg4", ".mpg", ".mts",
+        ".nsv",
+        ".ogm", ".ogv",
+        ".qt",
+        ".tod", ".ts",
+        ".vob",
+        ".wmv",
+    ]
+    return f[-1] in valid_video_formats
+
+
+def get_video_title(filename):
+    f = os.path.splitext(filename)
+    name = f[-2].replace("_", " ")
+    return name
+
+def convert_list_to_string(elements_list):
+    list_string = ""
+    for element in elements_list:
+        list_string += element+","
+    return list_string[:-1]
+
+
+def get_default_config():
+    config = open("./defaults.yaml")
+    params = yaml.load(config, Loader=yaml.FullLoader)
+    params['territories'] = convert_list_to_string(params['territories'])
+    params['whitelisted_fb_ids'] = convert_list_to_string(params['whitelisted_fb_ids'])
+    params['whitelisted_ig_ids'] = convert_list_to_string(params['whitelisted_ig_ids'])
+    params['tags'] = convert_list_to_string(params['tags'])
+    return params
+
+
+def scan_directory(directory):
+
+    print("Generating CSV file in ", directory)
+
+    output_file_name = directory.joinpath('video_ingest.csv')
+    csvfile = open(output_file_name, 'w+')
+
+    fieldnames = [
+        'Filename',
+        'Territories',
+        'Title',
+        'Description',
+        'Category',
+        'Monitoring Type',
+        'Match Rule ID',
+        'Reference Disabled',
+        'Whitelisted FB IDs',
+        'Whitelisted IG IDs',
+        'Tags',
+        'Action',
+    ]
+
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',')
+    writer.writeheader()
+
+    default_params = get_default_config()
+
+    for filename in os.listdir(directory):
+        if is_video_file(filename):
+            writer.writerow({
+                'Filename': filename,
+                'Territories': default_params['territories'],
+                'Title': get_video_title(filename),
+                'Description': default_params['description'],
+                'Category': default_params['category'],
+                'Monitoring Type': default_params['monitoring_type'],
+                'Match Rule ID': default_params['match_rule_id'],
+                'Reference Disabled': default_params['reference_disabled'],
+                'Whitelisted FB IDs': default_params['whitelisted_fb_ids'],
+                'Whitelisted IG IDs': default_params['whitelisted_ig_ids'],
+                'Tags': default_params['tags'],
+                'Action': default_params['action'],
+            })
+
+    csvfile.close()
+
+
+    print("CSV file generated in ", output_file_name)
+
+
+def main():
+    videos_dir_path = Path(input("Please introduce the full path to the directory that contains your videos: "))
+
+    while not videos_dir_path.exists():
+        videos_dir_path = Path(input("Invalid path, please try again: "))
+
+    scan_directory(videos_dir_path)
+
+
+main()
